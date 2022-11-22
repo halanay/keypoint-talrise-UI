dfdd401d8c32373a0030572a88cb03f9b6208534
Armagan <emeldursun.uk@gmail.com>
2022-11-22 13:17:11 +0000

    Language features are added.

--- src/test/java/talrise/pages/PageInitializer.java
+++ src/test/java/talrise/pages/PageInitializer.java
@@ -7,10 +7,11 @@ public class PageInitializer extends Driver {
     public static LoginPage loginPage;
     public static SignUpPage signUpPage;
 
+
     public static void initialize() {
         loginPage = new LoginPage();
         signUpPage = new SignUpPage();
+
     }
 
 }
-
